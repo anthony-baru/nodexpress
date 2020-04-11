80ce1a8bf17011b96bdadffd66f1cd93d0d0cc6c
anthony-baru <anthonybaru@gmail.com>
2020-04-11 13:57:59 +0300

    addedcontrollers and routes and exported accordingly

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const { server } = require('./config');
-const registerMiddlewares = require('./middlewares');
-
+// const registerMiddlewares = require('./middlewares');
+import registerMiddlewares from "./middlewares";
+import { rootRouter } from "./routes";
 async function main() {
   // TODO: register middlewares (CORS...)
   registerMiddlewares(server);
   // TODO: use router
-  server.get("/", (req, res, next) => {
-    res.json({ message: `Handling ${req.method} request.` })
-  });
-  server.post("/", (req, res, next) => {
-    res.json({ message: `Handling ${req.method} request.` })
-  });
-  server.put("/", (req, res, next) => {
-    res.json({ message: `Handling ${req.method} request.` })
-  });
-  server.delete("/", (req, res, next) => {
-    res.json({ message: `Handling ${req.method} request.` })
-  });
+  // server.get("/", (req, res, next) => {
+  //   res.json({ message: `Handling ${req.method} request.` })
+  // });
+  // server.post("/", (req, res, next) => {
+  //   res.json({ message: `Handling ${req.method} request.` })
+  // });
+  // server.put("/", (req, res, next) => {
+  //   res.json({ message: `Handling ${req.method} request.` })
+  // });
+  // server.delete("/", (req, res, next) => {
+  //   res.json({ message: `Handling ${req.method} request.` })
+  // });
   // TODO: default express error handling middleware
-
+  server.use("/v1", rootRouter);
   server.listen();
 }
 
