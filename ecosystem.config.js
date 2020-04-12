2654b27c3e6ba17501630acba9c2d2b6eb6215a8
anthony-baru <anthonybaru@gmail.com>
2020-04-12 17:23:07 +0300

    change on config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
       user: 'node',
       host: '212.83.163.1',
       ref: 'origin/master',
-      repo: 'antony-baru@github.com:nodexpress.git',
+      repo: 'git@github.com:repo.git',
       path: '/var/www/production',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
     }
